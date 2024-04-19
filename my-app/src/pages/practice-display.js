79ff51a160d835a2908a6c089328ac47ef78ba57
‘Abigail_Nielson’ <aa.nielson18@gmail.com>
2024-04-19 14:42:35 -0700

    prettier setup

---
+++ my-app/src/pages/practice-display.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PracticeDisplay = () => {
+  return (
+    <div className="main">
+      <h1>Practice Results</h1>
+      <div className="practice-results"></div>
+    </div>
+  )
+}
+
+export default PracticeDisplay
