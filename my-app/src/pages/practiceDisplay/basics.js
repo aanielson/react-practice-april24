238a5029d025facce10f6400d993caedfc459e06
‘Abigail_Nielson’ <aa.nielson18@gmail.com>
2024-04-19 15:49:18 -0700

    04192024 basic structure

--- my-app/src/pages/practiceDisplay/basics.js
+++ my-app/src/pages/practiceDisplay/basics.js
@@ -5,14 +5,28 @@ const BasicsJS = () => {
 }
 
 // 1. Write a JavaScript function to calculate the sum of two numbers.
+const calculateSum = (a, b) => {
+  return a + b
+}
 // 2. Write a JavaScript program to find the maximum number in an array.
+const maxNum = (array) => {
+  Math.max(array)
+}
 // 3. Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards).
+const isPalindrome = (string) => {}
 // 4. Write a JavaScript program to reverse a given string.
-//   5. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
+const reverseString = (string) => {}
+// 5. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
+const returnEvenNumbers = (array) => {}
 // 6. Write a JavaScript program to calculate the factorial of a given number.
+const findFactorial = () => {}
 // 7. Write a JavaScript function to check if a given number is prime.
+const isPrime = (num) => {}
 // 8. Write a JavaScript program to find the largest element in a nested array.
+const findLargestElement = (array) => {}
 // 9. Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms.
+const findFiboncci = (term) => {}
 // 10. Write a JavaScript program to convert a string to title case (capitalize the first letter of each word).
+const makeTitleCase = (string) => {}
 
 export default BasicsJS
